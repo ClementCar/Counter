f16e61903d55a0f1bebe44fa7478aa8de3cebfac
Clement Carcenac <clementcarcenac@gmail.com>
2024-12-16 16:13:59 +0100

    return

--- script.js
+++ script.js
@@ -1,28 +1,26 @@
-const targetTime = new Date('November 24, 2024 18:55:00').getTime();
-const targetJ = new Date('November 12, 2024 18:00:00').getTime();
+// const targetTime = new Date('January 16, 2025 22:00:00').getTime();
+const targetTime = new Date('2025-01-16T22:00:00+07:00').getTime();
 
 const days = document.getElementById('days');
-const daysJ = document.getElementById('daysJ')
 const hours = document.getElementById('hours');
-const hoursJ = document.getElementById('hoursJ');
 const minutes = document.getElementById('minutes');
-const minutesJ = document.getElementById('minutesJ');
 const seconds = document.getElementById('seconds');
-const secondsJ = document.getElementById('secondsJ');
 
 const countdown = setInterval(function() {
     const now = new Date().getTime();
     const distance = targetTime - now;
-    const distanceJ = targetJ - now;
 
     days.innerHTML = String(Math.floor(distance / (1000 * 60 * 60 * 24))).padStart(2, '0');
     hours.innerHTML = String(Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))).padStart(2, '0');
     minutes.innerHTML = String(Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60))).padStart(2, '0');
     seconds.innerHTML = String(Math.floor((distance % (1000 * 60)) / 1000)).padStart(2, '0');
-
-    daysJ.innerHTML = String(Math.floor(distanceJ / (1000 * 60 * 60 * 24))).padStart(2, '0');
-    hoursJ.innerHTML = String(Math.floor((distanceJ % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))).padStart(2, '0');
-    minutesJ.innerHTML = String(Math.floor((distanceJ % (1000 * 60 * 60)) / (1000 * 60))).padStart(2, '0');
-    secondsJ.innerHTML = String(Math.floor((distanceJ % (1000 * 60)) / 1000)).padStart(2, '0');
     
+    if (distance < 0) {
+        clearInterval(countdown);
+        days.innerHTML = "00";
+        hours.innerHTML = "00";
+        minutes.innerHTML = "00";
+        seconds.innerHTML = "00";
+    }
+
 }, 1000)
